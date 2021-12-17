4f077973c381d2040e40a12ef4ed89d14405a5f9
Lin153957 <200228kh@yse-c.net>
2021-12-17 09:17:15 +0900

    ...........

--- server.js
+++ server.js
@@ -1,18 +1,26 @@
 const express = require('express')
+const dotenv = require('dotenv')
 
-require('dotenv').config()
+dotenv.config()
 const host = process.env.HOST
 const port = process.env.PORT
 
+//routes.js を読み込む
+const routes = require('./routes')
+
 const app = express()
 
-app.get('/', (req, res) => {
-    res.send('Hello YSE!!!!')
-})
+app.use(express.static(__dirname + '/public'))
+app.use(express.urlencoded({ extended: true }))
 
-app.get('/profile', (req, res) => {
-    res.send('This is profile page')
-})
+const layouts = require('express-ejs-layouts')
+app.set('layout', 'layouts/default');
+app.set('view engine', 'ejs')
+app.use(layouts) ;
+
+//routes.js を使う
+app.use(routes)
+
 
 app.listen(port, host, () => {
     console.log('http://' + host + ':' + port)
